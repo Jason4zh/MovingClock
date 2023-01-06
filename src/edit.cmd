2f18bbab50fc969e3e0436841f03a591b7aa6253
Jason4zh <117495330+Jason4zh@users.noreply.github.com>
2023-01-06 21:51:38 +0800

    Delete src directory

--- src/edit.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd C:\Users\zrz19\Desktop\Python整活\我的pypi包\MovingClock
-python setup.py sdist build
-twine upload dist/*
-start https://github.com/Jason4zh/MovingClock/
-start https://pypi.org/project/MovingClock/
